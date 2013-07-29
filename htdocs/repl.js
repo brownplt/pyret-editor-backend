e7050f792b80f9eb1a7d5cc8363272c88347e8fc
Joe Gibbs Politz <joe@cs.brown.edu>
2013-07-29 10:28:17 -0400

    console info on language load fail

--- htdocs/repl.js
+++ htdocs/repl.js
@@ -112,6 +112,7 @@
             },
             function(err) {
                 console.error("Failed to load language: ", err);
+                console.log(err.message);
             });
     };
 
