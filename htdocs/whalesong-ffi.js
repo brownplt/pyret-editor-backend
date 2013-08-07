f1bc9587ceb0baa34b3c92365d6dfa377fc6971a
Joe Gibbs Politz <joe@cs.brown.edu>
2013-08-06 20:19:20 -0400

    ffi to call things in ws

---
+++ htdocs/whalesong-ffi.js
@@ -0,0 +1,48 @@
+(function(global) {
+  function callRacketFun(f, args, success, fail) {
+    plt.runtime.currentMachine.trampoline(function(M) {
+      plt.runtime.PAUSE(function(resume, internalCall) {
+        var wrappedSuccess = function(v) {
+          resume(function() { /* intentional no-op */ });
+          success(v);
+        };
+        var wrappedFail = function(v) {
+          resume(function() { /* intentional no-op */ });
+          fail(v);
+        };
+        internalCall.apply(null, [f, wrappedSuccess, wrappedFail].concat(args));
+      });
+    });
+  }
+
+  function getPyretLib(name) {
+    var f = plt.runtime.currentMachine.modules["root/lang/pyret-lang-whalesong.rkt"].exports.get(name);
+
+    if(!f) {
+      throw "Couldn't get Pyret fun: " + name;
+    } else {
+      return f;
+    }
+  }
+
+  function callPyretLibFun(name, args, success, fail) {
+    callRacketFun(getPyretFun(name), args, success, fail);
+  }
+
+  function callPyretFun(pFun, args, success, fail) {
+    var getApp = getPyretLib("p:p-base-app");
+    callRacketFun(getApp, [pFun], function(racketFun) {
+        callRacketFun(racketFun, args, success, fail);
+      },
+      function(e) {
+        console.error("Couldn't get Pyret fun: ", pFun, e);
+      }
+    );
+  }
+
+  global.whalesongFFI = {
+    callRacketFun: callRacketFun,
+    getPyretLib: getPyretLib,
+    callPyretFun: callPyretFun
+  };
+})(this);
